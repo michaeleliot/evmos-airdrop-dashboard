dbfe261abec4c72c48ed6c43cdc6fee36cc77ee3
Michael Eliot <mieliot@protonmail.com>
2022-01-12 09:11:43 -0800

    feature: add in two remaining endpoint stub functions

--- src/services/missionsService.ts
+++ src/services/missionsService.ts
@@ -1,6 +1,10 @@
 import axios from 'axios';
 import MissionData from 'src/assets/missiondata';
-import {CompletedTasksReturnObject, MissionsObject} from 'src/types';
+import {
+  CompletedTasksReturnObject,
+  LeaderBoardEntry,
+  MissionsObject,
+} from 'src/types';
 import {apiDomain} from '../const';
 
 export async function getMissionData(address: string): Promise<MissionsObject> {
@@ -9,6 +13,8 @@ export async function getMissionData(address: string): Promise<MissionsObject> {
   const completedTaskObject = Object.fromEntries(
     completedTasks.map(key => [key, true]),
   );
+
+  // Update what tasks they have completed. Might be worth moving this to react
   Object.entries(MissionData).forEach(input => {
     const [mission, tasks] = input;
     missionData[mission] = tasks.map(task =>
@@ -22,4 +28,37 @@ export async function getCompletedTasks(address: string): Promise<number[]> {
   const res = await axios.get(`${apiDomain}/user_missions/${address}`);
   const {data}: {data: CompletedTasksReturnObject} = res;
   return data.missions;
+}
+
+export async function getLeaderboardData(): Promise<LeaderBoardEntry[]> {
+  // TODO Get actual endpoint
+  return [
+    {
+      rank: 1,
+      walletAddress: '0x25dd91069f120d955803def92f2d1b32c2b471de',
+      points: 560,
+    },
+    {
+      rank: 2,
+      walletAddress: '0x25dd91061f220d955804def92f2d1b32c2b491dc',
+      points: 535,
+    },
+    {
+      rank: 3,
+      walletAddress: '0x25dd91069f120d955803def92f2d1b32c2b471de',
+      points: 510,
+    },
+    {
+      rank: 4,
+      walletAddress: '0x25dd91061f220d955804def92f2d1b32c2b491dc',
+      points: 495,
+    },
+  ];
+}
+
+export async function getRektDropInformation(
+  address: string,
+): Promise<number[]> {
+  // TODO Get actual endpoint
+  return [];
 }
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,1 +1,7 @@
 export * from './mission';
+
+export interface LeaderBoardEntry {
+  rank: number;
+  walletAddress: string;
+  points: number;
+}
